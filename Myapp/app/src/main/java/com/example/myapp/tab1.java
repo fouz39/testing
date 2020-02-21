6f21b82ac857246cb1f96096ae12dd1aa059a3cb
fouzAlrabai <60447225+fouzAlrabai@users.noreply.github.com>
2020-02-21 15:53:58 +0300

    time done

--- Myapp/app/src/main/java/com/example/myapp/tab1.java
+++ Myapp/app/src/main/java/com/example/myapp/tab1.java
@@ -5,7 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 
@@ -14,7 +16,7 @@ import androidx.fragment.app.Fragment;
  */
 public class tab1 extends Fragment {
 
-
+    TextView textView;
 
     public tab1() {
         // Required empty public constructor
@@ -25,7 +27,18 @@ public class tab1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_tab1, container, false);
+        textView = (TextView) view.findViewById(R.id.pickUpTime);
+        textView.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(getFragmentManager(), "TimePicker");
+            }
+
+        });
+        return view;
+
+    }
 }
